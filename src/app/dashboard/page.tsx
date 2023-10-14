cc55dc4d05728e938089b91348d0abbd512066a7
hanggarahadianto <93454559+hanggarahadianto@users.noreply.github.com>
2023-10-15 02:18:59 +0700

    remove lotte

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,14 +5,14 @@ import RestaurantCard from "@/components/restaurant/restaurantCard";
 import { RestaurantMaping } from "@/types/restaurant";
 import { getRestaurantData } from "@/utils/api/restaurant.api";
 import React from "react";
-import DasboardSection from "@/components/section/DasboardSection";
+// import DasboardSection from "@/components/section/DasboardSection";
 
 export default async function DashboardPage() {
   const restaurantData: RestaurantMaping[] = await getRestaurantData();
   return (
     <div suppressHydrationWarning={true}>
       <LandingPage />
-      <DasboardSection />
+      {/* <DasboardSection /> */}
       <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-8 py-8">
         {restaurantData.map((Restaurant) => (
           <div key={Restaurant.ID}>
